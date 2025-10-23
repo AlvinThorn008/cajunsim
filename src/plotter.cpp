f72277c1e566e9fa8bdc690a1af4aa739b1e907c
AlvinThorn008 <xconlives@gmail.com>
2025-10-23 12:29:45 +0100

    Add: port c2 to cpp.
    Encapsulated plotter in ForuierPlotter class.

---
+++ src/plotter.cpp
@@ -0,0 +1,76 @@
+#include "plotter.hpp"
+
+#include <iostream>
+
+// https://stackoverflow.com/questions/4217037/catch-ctrl-c-in-c
+static volatile sig_atomic_t running = 1;
+static void ctrl_c_handler(int _) {
+    (void)_;
+    running = 0;
+}
+
+FourierPlotter::FourierPlotter(size_t coef_count, double base_frequency):
+    max_amplitude(0.0),
+    f0(base_frequency), cos_head(0), 
+    sin_head(coef_count), coef_count(coef_count) { 
+    coefs.resize(2 * coef_count, 0.0);
+}
+
+void FourierPlotter::append_cos_coef(double val) {
+    if (cos_head == coef_count) return;
+    if (val >= max_amplitude) max_amplitude = val;
+    coefs[cos_head++] = val;
+}
+
+void FourierPlotter::append_sin_coef(double val) {
+    if (sin_head == 2 * coef_count) return;
+    if (val >= max_amplitude) max_amplitude = val;
+    coefs[sin_head++] = val;
+}
+
+double FourierPlotter::sample(double x) {
+    double value = 0.0;
+    for (size_t i = 0; i < coef_count; i++) {
+        value += coefs[i] * cos(i * f0 * x * PI/180.0) 
+                + coefs[i + coef_count] * sin(i * f0 * x * PI/180.0);
+    }
+    return value;
+}
+
+void FourierPlotter::start_plotter(size_t num_points) {
+    // Handling ctrl-c allows the program to reset the terminal before exiting
+    signal(SIGINT, ctrl_c_handler);
+
+    long x = 0;
+    float y, y_1, y_2;
+    int out[3];
+    double y_min = INFINITY, y_max = 0.0;
+
+    while (num_points-->0 && running) {
+        x += 1;
+        printf("x = " ORG "%0*d" RESET, 8, x);
+
+        y = (sample((double)x)) / max_amplitude;
+        printf(", y = " ORG "%.2f" RESET "|", y);
+
+        if (x % TICK_INTERVAL == 0) printf(RED "---" RESET);
+        else printf("   ");
+
+        lerp(c1, c3, out, y);
+        printf("\x1b[48;2;%d;%d;%dm", out[0], out[1], out[2]);
+        plotval(y, PLOT_WIDTH);
+        printf(RESET "\n");
+    }
+    running = 1;
+    printf("\n\033[0m");
+}
+
+void plotval(float d, int width) {
+    printf("%*s", (int)(d * width) + 1, "*");
+}
+
+void lerp(const int c1[3], const int c2[3], int out[3], float t) {
+    out[0] = (int) ((c2[0] - c1[0]) * t + c1[0]);
+    out[1] = (int) ((c2[1] - c1[1]) * t + c1[1]);
+    out[2] = (int) ((c2[2] - c1[2]) * t + c1[2]);
+}
---
+++ src/plotter.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <cstdio>
+#include <vector>
+#include <csignal>
+#include <cmath>
+#include <tuple>
+
+#define PI 3.1415926535897932384626433f
+
+// Source: https://stackoverflow.com/a/23657072
+#define RED   "\x1B[31m"
+#define GRN   "\x1B[32m"
+#define YEL   "\x1B[33m"
+#define BLU   "\x1B[34m"
+#define MAG   "\x1B[35m"
+#define CYN   "\x1B[36m"
+#define WHT   "\x1B[37m"
+#define RESET "\x1B[0m"
+
+#define ORG "\x1B[38;2;255;143;79m"
+
+// Console width available for '*'s to be printed.
+const int PLOT_WIDTH = 160;
+// How frequently should graduations appear
+const int TICK_INTERVAL = 10;
+const int c1[3] = {255, 102, 0};
+const int c3[3] = {198, 120, 221};
+
+class FourierPlotter {
+    std::vector<double> coefs;
+    size_t coef_count;
+    size_t cos_head;
+    size_t sin_head;
+    double f0;
+    double max_amplitude;
+    
+    public:
+    FourierPlotter(size_t coef_count, double base_frequency);
+
+    void append_cos_coef(double val);
+    void append_sin_coef(double val);
+    double sample(double x);
+    void start_plotter(size_t num_points);
+};
+
+void plotval(float d, int width);
+void lerp(const int c1[3], const int c2[3], int out[3], float t);
